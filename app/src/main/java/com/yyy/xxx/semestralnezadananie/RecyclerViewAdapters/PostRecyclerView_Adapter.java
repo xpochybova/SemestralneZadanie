ee414c6fe952b4c53d6af95e77877214ed606589
CodeForLife <timspik1@gmail.com>
2018-12-06 15:34:28 -0800

    videoplayer

--- app/src/main/java/com/yyy/xxx/semestralnezadananie/RecyclerViewAdapters/PostRecyclerView_Adapter.java
+++ app/src/main/java/com/yyy/xxx/semestralnezadananie/RecyclerViewAdapters/PostRecyclerView_Adapter.java
@@ -1,5 +1,6 @@
 package com.yyy.xxx.semestralnezadananie.RecyclerViewAdapters;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -9,6 +10,7 @@ import android.view.ViewGroup;
 import com.yyy.xxx.semestralnezadananie.Entities.Post;
 import com.yyy.xxx.semestralnezadananie.Entities.User;
 import com.yyy.xxx.semestralnezadananie.R;
+import com.yyy.xxx.semestralnezadananie.VideoPlayer.VideoPlayer;
 
 import java.util.List;
 
@@ -17,9 +19,15 @@ public class PostRecyclerView_Adapter extends RecyclerView.Adapter<RecyclerView.
     private List<Post> prvky;
     private User profil;
 
+    private VideoPlayer videoPlayer;
+
+    private Context context;
+
     public PostRecyclerView_Adapter(List<Post> prvky, User profil){
         this.prvky = prvky;
         this.profil = profil;
+
+
 
         // VYTVORI PLACEHOLDER PRE PROFIL NA NULTEJ POZICII
         if (prvky.get(0) != null)
@@ -30,6 +38,8 @@ public class PostRecyclerView_Adapter extends RecyclerView.Adapter<RecyclerView.
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i)
     {
+        context = viewGroup.getContext();
+
         if (i == 0)
         {
             View itemView = LayoutInflater.from(viewGroup.getContext())
@@ -60,6 +70,10 @@ public class PostRecyclerView_Adapter extends RecyclerView.Adapter<RecyclerView.
             // TODO: 25.11.2018 setnut view pre prispevoky
             PostRecyclerView_ViewHolder h = (PostRecyclerView_ViewHolder)holder;
             h._post.setText(prvky.get(i).toString());
+
+            videoPlayer = new VideoPlayer(h.playerView);
+            videoPlayer.playVideo("",context);
+
         }
     }
 
--- app/src/main/java/com/yyy/xxx/semestralnezadananie/RecyclerViewAdapters/PostRecyclerView_ViewHolder.java
+++ app/src/main/java/com/yyy/xxx/semestralnezadananie/RecyclerViewAdapters/PostRecyclerView_ViewHolder.java
@@ -5,15 +5,19 @@ import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.TextView;
 
+import com.google.android.exoplayer2.ui.PlayerView;
 import com.yyy.xxx.semestralnezadananie.R;
 
 public class PostRecyclerView_ViewHolder extends RecyclerView.ViewHolder {
     public TextView _post;
+    PlayerView playerView;
+
 
     public PostRecyclerView_ViewHolder(@NonNull View itemView) {
         super(itemView);
 
         _post = itemView.findViewById(R.id.recyclerview_post_vertical);
+        playerView = itemView.findViewById(R.id.playerView);
 
     }
 }
